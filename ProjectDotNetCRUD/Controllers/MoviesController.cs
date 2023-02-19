e29e595fa8555f7621c6c2d48d547f12709cb616
hamoudi8080 <hamoderaso@outlook.com>
2023-02-19 19:53:08 +0100

    Initial commit

---
+++ ProjectDotNetCRUD/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ProjectDotNetCRUD.Models;
+using ProjectDotNetCRUD.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectDotNetCRUD.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        { 
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //MY Action is index i retrive all data from database and 
+            var movies = await _context.Movies.ToListAsync();
+           // then it sends them to the view in shared.Index
+            return View(movies);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync()
+            };
+            return View(viewModel);
+        }
+    }
+}
